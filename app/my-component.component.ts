c05d482fdca0b48d229501fb5e4523e80a66b95b
hrc099 <hrc099@gmail.com>
2016-11-03 14:41:22 +0100

    Add files via upload
    Created simple Angular 2 quickstart app, added another component and some variables and functions in it, playing with data binding and events

---
+++ app/my-component.component.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-component',
+    template: `
+        <h1>{{title}}</h1>
+        <img [src]="imageLink">
+        <br>
+        <button (click)="runClick()">Click</button>
+        <input type="text" [(ngModel)]="note" (keyup.enter)="saySomething($event)">
+        {{note}}
+        `
+})
+
+export class MyComponent {
+    private title;
+    private imageLink;
+    private note;
+
+    constructor() {
+        this.title = 'Hello world!';
+        this.imageLink = 'http://lorempixel.com/400/200';
+        this.note;
+    }
+
+    runClick() {
+        console.log('Click!');
+    }
+
+    saySomething(e) {
+        console.log(e.target.value)
+    }
+}
